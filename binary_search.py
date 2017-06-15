b76da8be4c489ce85667eca0dea305f10f81b3ad
James Kinyua <jameskinyua590@gmail.com>
2017-06-15 12:09:55 +0300

    Add a .gitignore file

---
+++ binary_search.py
@@ -0,0 +1,40 @@
+def binary_search(a_list, item):
+    """
+    Binary search algorithm.
+    """
+    first = 0
+    last = len(a_list)-1
+    found = False
+    
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+    
+    return found
+    
+
+class BinarySearch(list):
+    """
+    This class inherits from the list class and takes two integers as parameters,
+    a and b. a is the length of the list to be created and  b is the step or
+    difference between consecutive values and initializes an instance variable
+    length.
+    It defines a method search which implements the binary search algorithm and
+    takes in the value you are to find as its argument. It returns a dictionary
+    object which contains count, the number of times the function iterated and
+    index, which is the index of the item in the list.
+    """
+    def __init__(self, a, b):
+        num = b
+        for i in range(a):
+            self.append(num)
+            num += b
+        self.length = len(self)
+        self.sort() # Sort the list to implement binary search algorithm.
+        
